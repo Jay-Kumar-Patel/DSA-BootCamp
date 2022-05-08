740221276fb5ebe7d624af57230fbb5f805ac0a8
Jay <jaypatel41120@gmail.com>
2022-05-08 09:43:43 +0530

    commit

--- Tree/Univalued Binary Tree.java
+++ Tree/Univalued Binary Tree.java
@@ -14,3 +14,30 @@ class Solution {
         return checkUnivalTree(root.left,val) && checkUnivalTree(root.right,val);
     }
 }
+
+
+//Using BFS
+/*class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        int requiredValue = root.val;
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            
+            TreeNode currNode = queue.remove();
+            
+            if(currNode.val != requiredValue)
+                return false;
+            
+            if(currNode.left != null)
+                queue.add(currNode.left);
+            
+            if(currNode.right != null)
+                queue.add(currNode.right);
+            
+        }
+        return true;
+    }
+} */
--- Tree/height of binary tree.java
+++ Tree/height of binary tree.java
@@ -15,3 +15,40 @@ class Solution {
     }
     
 }
+
+
+//Using BFS
+/*class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        int height = 0;
+        
+        if(root == null)
+            return height;
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            
+            int currLevel = queue.size();
+            
+            while(currLevel > 0){
+                
+                TreeNode currNode = queue.remove();
+                
+                if(currNode.left != null)
+                    queue.add(currNode.left);
+                
+                if(currNode.right != null)
+                    queue.add(currNode.right);
+                
+                currLevel -= 1;
+            }
+            
+            height += 1;
+        }
+        
+        return height;
+    }
+} */
