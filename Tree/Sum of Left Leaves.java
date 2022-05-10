57ccb32d1dc5ef4462d142ac5146ec8c928eca1c
Jay <jaypatel41120@gmail.com>
2022-05-10 11:41:46 +0530

    commit

--- Tree/Sum of Left Leaves.java
+++ Tree/Sum of Left Leaves.java
@@ -21,3 +21,43 @@ class Solution {
         return leftAns + rightAns;
     }
 }
+
+
+//Using BFS
+/*
+class Pair{
+    TreeNode node;
+    boolean isLeftNode;
+    
+    Pair(TreeNode node, boolean isLeftNode){
+        this.node = node;
+        this.isLeftNode = isLeftNode;
+    }
+}
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+        Queue<Pair> queue = new LinkedList<>();
+        int ans = 0;
+        
+        queue.add(new Pair(root,false));
+        
+        while(!queue.isEmpty()){
+            Pair currPair = queue.remove();
+            TreeNode currNode = currPair.node;
+            boolean isLeft = currPair.isLeftNode;
+            
+            if(currNode.left == null && currNode.right == null && isLeft == true){
+                ans += currNode.val;
+            }
+            
+            if(currNode.left!=null)
+                queue.add(new Pair(currNode.left,true));
+            if(currNode.right!=null)
+                queue.add(new Pair(currNode.right,false));
+        }
+        
+        return ans;
+    }
+} */
