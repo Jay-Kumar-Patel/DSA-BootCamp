180e513d4442b9ab0cdf816899717b274898c321
Jay Patel <103202027+Jay-Kumar-Patel@users.noreply.github.com>
2022-07-01 10:42:52 +0530

    Commit

--- Dynamic Programming/Partition Equal Subset Sum.java
+++ Dynamic Programming/Partition Equal Subset Sum.java
@@ -1,3 +1,4 @@
+//Java Code
 class Solution {
     public boolean canPartition(int[] nums) {
         
@@ -46,3 +47,53 @@ class Solution {
         return consider||notconsider;
     }
 }
+
+
+
+
+
+//C++ Code
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+        }
+        
+        if(sum%2 != 0)
+            return false;
+        
+        unordered_map<string, bool> memo;
+        return checkPartition(0,nums,memo,sum/2);
+    }
+    
+    bool checkPartition(int currIndex, vector<int>& nums, unordered_map<string,bool>& memo, int sum){
+        
+        if(currIndex >= nums.size())
+            return false;
+            
+        if(sum == 0)
+            return true;
+        
+        string currentKey = to_string(currIndex) + "-" + to_string(sum);
+        
+        if(memo.find(currentKey) != memo.end())
+            return memo[currentKey];
+        
+        bool consider = false;
+        if(sum >= nums[currIndex])
+            consider = checkPartition(currIndex+1, nums, memo, sum-nums[currIndex]);
+        
+        if(consider == true){
+            memo[currentKey] = true;
+            return true;
+        }
+        
+        bool notConsider = checkPartition(currIndex+1, nums, memo, sum);
+        
+        memo[currentKey] = (consider || notConsider);
+        return (consider || notConsider);
+    }
+};
