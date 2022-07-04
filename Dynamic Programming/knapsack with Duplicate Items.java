3b20b2121b48ae4c95ce2075a487e44c1e47f256
Jay Patel <103202027+Jay-Kumar-Patel@users.noreply.github.com>
2022-07-04 15:36:31 +0530

    Commit

--- Dynamic Programming/knapsack with Duplicate Items.java
+++ Dynamic Programming/knapsack with Duplicate Items.java
@@ -1,3 +1,4 @@
+//Java Code
 class Solution{
     public int knapSack(int N, int W, int val[], int wt[])
     {
@@ -34,3 +35,36 @@ class Solution{
         return Math.max(consider,notconsider);
     }
 }
+
+
+
+
+
+//C++ Code
+class Solution{
+public:
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        vector<vector<int>> memo(N, vector<int>(W+1,-1));
+        return maxProfit(0,W,val,wt,memo,N);
+    }
+    
+    int maxProfit(int currIndex, int capacity, int val[], int wt[], vector<vector<int>>& memo, int N){
+        
+        if(currIndex == N)
+            return 0;
+    
+        if(memo[currIndex][capacity] != -1)
+            return memo[currIndex][capacity];
+            
+        int consider = 0;
+        if(capacity >= wt[currIndex]){
+            consider = val[currIndex] + maxProfit(currIndex,capacity-wt[currIndex],val,wt,memo,N);
+        }
+        
+        int notConsider = maxProfit(currIndex+1,capacity,val,wt,memo,N);
+        
+        memo[currIndex][capacity] = max(consider, notConsider);
+        return max(consider, notConsider);
+    }
+};
